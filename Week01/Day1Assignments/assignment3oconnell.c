a4325f74ecac801d4b618400d9d9cf8d540759cd
Amethyst O'Connell <oconnellamethyst@gmail.com>
2018-05-30 14:36:58 -0500

    Added Lecture 2 Notes, Updated Assignment 3

--- Week01/Day1Assignments/assignment3oconnell.c
+++ Week01/Day1Assignments/assignment3oconnell.c
@@ -1,46 +1,47 @@
 // assignment3oconnell.c
 #include <stdio.h>
-int main(void)                /* a program that does prism computations with three numbers */
-{
-	int height = 20;
-	int depth = 20;
-	int width = 20;
-	
-	int sat; // Surface area of top
-	int saff; // of front face
-	int sae; // of end
-	int sap; // and the whole prism
-	sat = depth * width;
-	saff = height * width;
-	sae = depth * height;
-	sap = 2 * (sat + saff + sae);
-	
-    printf("Surface Areas: \n");
-    printf("Top: %d,",sat);
-    printf(" Front face: %d,",saff);
-    printf(" End: %d,",sae);
-    printf(" Whole Prism: %d\n",sap);
-    
-    int pb; //Perimeter bottom
-    int pff; // front face
-    int pe; // and end
-    pb = 2 * (depth + width);
-    pff = 2 * (height + width);
-    pe = 2 * (height + depth);
-    
-    printf("Perimeters: \n");
-    printf("Bottom: %d,",pb);
-    printf(" Front face: %d,",pff);
-    printf(" End: %d\n",pe);
-    
-    int v; //volume
-    v = height * depth * width;
-    printf("Volume: %d \n", v);
-    
-    int d; //diagonal
-    d = sqrt(depth*depth + height*height + width*width);
-    printf("Diagonal through the center of the prism: %d\n",d);
+#include <math.h>
 
+int main()                /* a program that gets three numbers and does prism computations with three numbers */
+{
+	float height, depth, width;
+	float sat, saff, sae, sap; // Surface area of top, of front face, of end, and the whole prism
+	float v; // Volume
+	float pb, pff, pe; // Perimeter bottom, front face, and end
+	float d; // Diagonal
+	
+	printf("What is the height?\n");
+	scanf("%f", &height);
+	printf("What is the depth?\n");
+	scanf("%f", &depth);
+	printf("What is the height?\n");
+	scanf("%f", &width);
+	
+	sat = depth * width; // Calculates Surface area of top
+	saff = height * width; // of front face
+	sae = depth * height; // of end
+	sap = 2 * (sat + saff + sae); // and the whole prism
+	
+    printf("\nSurface Areas: \n"); // Prints the surface areas all nice
+    printf("Top: %.2f,",sat);
+    printf(" Front face: %.2f,",saff);
+    printf(" End: %.2f,",sae);
+    printf(" Whole Prism: %.2f\n",sap);  
+    
+    pb = 2 * (depth + width); // Perimeter of bottom
+    pff = 2 * (height + width); // front face
+    pe = 2 * (height + depth); // and end
+    
+    printf("\nPerimeters: \n"); // Prints the perimeters all nice
+    printf("Bottom: %.2f,",pb);
+    printf(" Front face: %.2f,",pff);
+    printf(" End: %.2f\n",pe);
+    
+    v = height * depth * width; // Volumes
+    printf("\nVolume: %.2f \n", v); // Prints the volume
+    
+    d = sqrtf(depth*depth + height*height + width*width); // Diagonal
+    printf("\nDiagonal through the center of the prism: %.2f\n",d); // Prints the diagonal
     
     return 0;
 }
--- Week01/README.md
+++ Week01/README.md
@@ -6,6 +6,7 @@ Compile with ```-Wall``` and it will give you all the warnings
 ## Lecture 2
 
 There are different types of data types  
+   
 **Integers**  
 char  
 short  
@@ -30,4 +31,51 @@ There are comments in C
  */
  
 // This is a single line comment, it is only on a single line.
+```
+  
+Because C has heavy customization, C is used a lot in microprocessors.
+  
+
+```C
+#include <stdio.h> // This is preprocessor instructions
+
+int main() // There is always a main in a C program
+{
+	printf("Here is a program \n");  // It's my program
+	
+	return 0; // Here's the return statement, a lot like Python!
+}
+```
+  
+C runs the main first!  
+  
+C has ways to deal with numbers!
+
+```C
+int x = 54; // this is a decimal number
+x = 066; // this is an octal number
+x = 0x36 // this is a hexadecimal number
+printf("x = %d in decimal",x);
+printf("x = %o in octal",x);
+printf("x = %0x in hexadecimal",x);
+```
+
+Conversion specifiers are found on Page 6 of the AppendixC pdf Document  
+  
+C identifiers (variable names) are case sensitive, must consist of letters, digits, and underscores, first character must be a letter, and although there is no length limit, it only looks at the first 31-63 characters. Keywords should not be used, keywords are found on page 10 of the AppendixC pdf Document.  
+  
+```C
+sizeof(int) // Allows you to find out how big it is, because int is different sizes on different computers
+```
+  
+There is an input function, ```scanf()``` to accompany the output function ```printf()```
+```C
+scanf("%f", &height); // You've got to specify the spot to put the stuff
+```
+  
+Java is known for being slow, because it does a whole bunch of stuff. C doesn't so it's fast.
+  
+```C
+#include <math.h>
+sqrtf(float); // Takes the square root of the floating point number float
 ```
