bb3709f3017eaa6c0a3fa6d15f5e3727c00be739
Amethyst O'Connell <oconnellamethyst@gmail.com>
2018-06-18 16:31:11 -0500

    Assignment 6 + drool

--- Week04/Day1Assignments/assignment6oconnell.c
+++ Week04/Day1Assignments/assignment6oconnell.c
@@ -1,60 +1,60 @@
 /*     FILE: assignment6oconnell.c     */
 
 /*
-   Takes a file with a bunch of numbers in it and computes things
+   Takes a file from the command line with a bunch of numbers in it and computes the Standard Devation, the Average, and the biggest number
 */
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
-
-int* sort(int* file);
+#include <limits.h>
 
 int main(int argc, char *argv[])
 {
   FILE *numbers;
-  numbers = fopen(argv[0], "r");
+  numbers = fopen(argv[1], "r"); // Reads the second argument passed in, which should be a file name
   
-  int numArray, great, i;
-  numArray = sort(numbers);
-  numArray[0] = great;
+  int hold;
+  int holdGreat = INT_MIN; // Starts with the smallest number so that there is no possible smaller number
+  int holdSquared = 0;
+  int holdSum = 0;
+  int n = 0;
+  double avg, stdDev;  
   
-  
-  printf("The highest value is %d\n",great);
-  
-  for(i = 0, i<sizeof(numArray), i++){
-  	
-  }
+  if(numbers != NULL){
+    while(fscanf(numbers,"%d", &hold) == 1){
+    	if(hold > holdGreat){ // holdGreat is always holding the greatest number. It's initialized as the smallest possible number, INT_MIN
+    		holdGreat = hold;
+		}
+		else{
+			; // Do nothing
+		}
+	    
+		holdSquared += (hold * hold); // holdSquared is holding the addition of all the stuffs squared
+		holdSum += hold; // A running sum
+		n++;
+	}
+	}
+	else{
+		printf("Unable to open file.\n");
+	}
+    
+	stdDev = sqrt(((double)(n*holdSquared - (holdSum*holdSum)))/((double)(n*(n-1)))); // Standard deviation calculation
+	avg = ((double)holdSum)/((double)n); // Average calculation
+	
+	printf("The highest value is %d\n",holdGreat); // Outputs
+	printf("The average is %.2f\n",avg);
+	printf("The Standard Deviation is %.6f\n",stdDev);
+	
+    fclose(numbers); 
   
   
   return 0;
 }
 
-int* sort(int* file){ // This is the selection sort program select.c adapted for this program
-  int scores[] = file;
-  int size = sizeof(scores);
-  int n, pass, item, position, temp;
-
-  /* Get number of values to read */
-  printf("Please enter number of scores (%d or less): ", size);
-  scanf("%d", &n);
-
-    /* Selection-sort the values read in. */
-    for(pass=0; pass<n-1; pass++){
-      position = pass;
-      for(item=pass+1; item<n; item++)
-        if (scores[position] < scores[item])
-          position = item;
-      if(pass != position){
-        temp = scores[pass];
-        scores[pass] = scores[position];
-        scores[position] = temp;
-      }
-    }
-
-    return &scores;
-}
-
 /*    OUTPUT: assignment6oconnell.c
+assignment6oconnell test1.dat
 
+  The highest value is 9
+  The average is 6.60
+  The Standard Deviation is 2.073644
   
 */
--- Week04/README.md
+++ Week04/README.md
@@ -1,2 +1,36 @@
-# csci1113
-C/C++ Programming for Scientists and Engineers at Normandale
+# csci1113 Lecture Notes
+## Lecture 1
+Everything in C works in C++  
+Keyword list is bigger, pg 6 C++.pdf  
+Same basic structure, but C has a better way of generating output.
+```C++
+#include <iostream> // Replaced stdio, you can still use stdio though
+using namespace std; // Allows use of cout instead of blahblahsirtypesalot
+
+int main( )
+{
+  cout << "Hello world!";
+  return 0;
+}
+```
+For using C libs, you can also do ```<cstdio>``` to emphasize the C ness.  
+Also ```endl = \n``` so ```cout << x << endl``` prints whatevers in x and then a new line.  
+C does integer constants by default.  
+```C++
+x = 0xAF;
+cout << "x = " << x << endl;
+x = 0257;
+cout << "x = " << x << endl;
+x = 175;
+cout << "x = " << x << endl;
+```
+The ```const``` keyword makes it a read-only variable which means the compiler has your back, like ```#define``` in C, but more powerful, protects you from yourself (but not from hackers).  
+Type conversion is done with function notation, ie ```float(x)``` means ```(float)x``` in C++  
+In C++ functions like sizeof return a double, but format can give you something else, C++.pdf pg 16  
+C++ can do structs, pg 20 C++.pdf  
+C++ has inline functions, which drops the code right in for a function, can help with speed, pg 24, Inline functions need the whole function definition  
+The C++ compiler is called on the command line using ```g++ program.cpp``` it defaults to a just like C. You can set up a make file and do things.  
+C++ has reference parameters, pg 34, which let you do things like the pointer swap function, but it does the work for you.  
+C++ we make the compiler do more work  
+pg 45, we can do weird things?  
+
